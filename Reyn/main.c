c4e38e03dfd9db0328dc45e6311ac59468586389
Reyn <117028090+Avereyy@users.noreply.github.com>
2024-06-06 20:39:00 +0700

    Update main.c

--- Reyn/main.c
+++ Reyn/main.c
@@ -53,11 +53,22 @@ void hapusNewline(char *str) {
 
 void tambahPasien(Pasien pasien[], int *jumlah_pasien) {
     Pasien new_pasien;
+    int nomor_valid = 0;
 
-    printf("Masukkan Nomor: ");
-    scanf("%d", &new_pasien.no);
-    getchar(); 
+    while (!nomor_valid) {
+        printf("Masukkan Nomor: ");
+        scanf("%d", &new_pasien.no);
+        getchar(); 
 
+        nomor_valid = 1;
+        for (int i = 0; i < *jumlah_pasien; i++) {
+            if (pasien[i].no == new_pasien.no) {
+                printf("Nomor pasien sudah ada, masukkan nomor yang lain.\n");
+                nomor_valid = 0;
+                break;
+            }
+        }
+    }
     printf("Masukkan Nama Lengkap: ");
     fgets(new_pasien.nama, sizeof(new_pasien.nama), stdin);
     hapusNewline(new_pasien.nama);
@@ -109,6 +120,7 @@ void tambahPasien(Pasien pasien[], int *jumlah_pasien) {
             new_pasien.id_pasien);
 
     fclose(file);
+    
 
     printf("Pasien berhasil ditambahkan.\n");
 }
@@ -182,6 +194,92 @@ void cariPasien(Pasien pasien[], int jumlah_pasien) {
     printf("Nomor pasien tidak ditemukan.\n");
 }
 
+void menuData(Pasien pasien[], int *jumlah_pasien) {
+    int pilihan;
+    do {
+        printf("\nAkses Data Pasien\n");
+        printf("1. Tambah Pasien\n");
+        printf("2. Hapus Pasien\n");
+        printf("3. Cari Pasien\n");
+        printf("4. Ubah Data Pasien\n");
+        printf("5. Kembali ke Menu Utama\n");
+        printf("Pilihan: ");
+        scanf("%d", &pilihan);
+
+        switch (pilihan) {
+            case 1:
+                tambahPasien(pasien, jumlah_pasien);
+                break;
+            case 2:
+                hapusPasien(pasien, jumlah_pasien);
+                break;
+            case 3:
+                cariPasien(pasien, *jumlah_pasien);
+                break;
+            case 4:
+                break;
+            case 5:
+                printf("Kembali ke Menu Utama..\n");
+                break;
+            default:
+                printf("Pilihan tidak valid.\n");
+        }
+    } while (pilihan != 4);
+}
+
+void menuRiwayat() {
+    int pilihan;
+    do {
+        printf("\nAkses Riwayat Pasien\n");
+        printf("1. Tambah Riwayat\n");
+        printf("2. Hapus Riwayat\n");
+        printf("3. Ubah Riwayat\n");
+        printf("4. Cari Riwayat\n");
+        printf("5. Kembali ke Menu Utama\n");
+        printf("Pilihan: ");
+        scanf("%d", &pilihan);
+
+        switch (pilihan) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                printf("Kembali ke Menu Utama..\n");
+                break;
+            default:
+                printf("Pilihan tidak valid.\n");
+        }
+    } while (pilihan != 3);
+}
+
+void menuIncome() {
+    int pilihan;
+    do {
+        printf("\nAkses Data Rumah Sakit\n");
+        printf("1. Tambah Data\n");
+        printf("2. Lihat Data\n");
+        printf("3. Kembali ke Menu Utama\n");
+        printf("Pilihan: ");
+        scanf("%d", &pilihan);
+
+        switch (pilihan) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                printf("Kembali ke Menu Utama..\n");
+                break;
+            default:
+                printf("Pilihan tidak valid.\n");
+        }
+    } while (pilihan != 3);
+}
 
 int main() {
     Pasien pasien[100];
@@ -191,25 +289,24 @@ int main() {
     bacaCSV(pasien, &jumlah_pasien);
 
     do {
-        printf("\nMenu\n");
-        printf("1. Tambah Pasien\n");
-        printf("2. Hapus Pasien\n");
-        printf("3. Cari Pasien\n");
+        printf("\nMenu Utama\n");
+        printf("1. Akses Data Pasien\n");
+        printf("2. Akses Riwayat Pasien\n");
+        printf("3. Akses Data Rumah Sakit\n");
         printf("4. Keluar\n");
         printf("Pilihan: ");
         scanf("%d", &pilihan);
 
         switch (pilihan) {
             case 1:
-                tambahPasien(pasien, &jumlah_pasien);
+                menuData(pasien, &jumlah_pasien);
                 break;
             case 2:
-                hapusPasien(pasien, &jumlah_pasien);
+                menuRiwayat();
                 break;
             case 3:
-                cariPasien(pasien, jumlah_pasien);
+                menuIncome();
                 break;
- 
             case 4:
                 printf("Keluar..\n");
                 break;
