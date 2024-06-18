fed0ab783fe2326692f4b28241e9160b486fc7a2
Reyn <117028090+Avereyy@users.noreply.github.com>
2024-06-19 00:10:50 +0700

    Add files via upload

---
+++ Source Code/Integrasi/diagnosislib.h
@@ -0,0 +1,18 @@
+#ifndef DIAGNOSISLIB_H
+#define DIAGNOSISLIB_H
+
+#include "main.h"
+
+int parse_bulan(const char *bulan_str);
+
+void parse_tanggal(const char *tanggal_str, int *tahun, int *bulan);
+
+void sort_data_bulanan(MonthlyDiagnosisData *monthly_Data, int size);
+
+void count_pasien_tahunan(MonthlyDiagnosisData *monthly_Data, int monthly_Data_Size, YearlyDiagnosisData *yearly_Data, int *yearly_Data_Size);
+
+void sort_data_tahunan(YearlyDiagnosisData *yearly_Data, int size);
+
+void menuRiwayatDiagnosis();
+
+#endif // DIAGNOSISLIB_H
---
+++ Source Code/Integrasi/main.h
@@ -0,0 +1,74 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_RECORDS 1000
+#define MAX_STRING 100
+#define MAX_LINE_LENGTH 1024
+#define MAX_PASIEN 1000
+#define MAX_DIAGNOSIS 100
+#define MAX_BULAN 12
+#define MAX_TAHUN 5
+
+
+typedef struct {
+    int no;
+    char nama[50];
+    char alamat[50];
+    char kota[20];
+    char tempat_lahir[20];
+    char tanggal_lahir[30];
+    int umur;
+    long long no_bpjs;
+    char id_pasien[20];
+} Pasien;
+
+typedef struct {
+    char tanggal[20];
+    int biaya;
+} Record;
+
+typedef struct
+{
+    int no;
+    char tanggal[MAX_STRING];
+    char id_pasien[MAX_STRING];
+    char diagnosis[MAX_STRING];
+    char tindakan[MAX_STRING];
+    char kontrol[MAX_STRING];
+    char biaya[MAX_STRING];
+} Riwayat;
+
+typedef struct {
+    int tahun;
+    int bulan;
+    char diagnosis[100];
+} Diagpasien;
+
+typedef struct {
+    char diagnosis[100];
+    int count;
+} DiagnosisCount;
+
+typedef struct {
+    int tahun;
+    int bulan;
+    DiagnosisCount diagnosisCounts[MAX_DIAGNOSIS];
+    int diagnosisCountSize;
+    int total_pasien; // Total patients for this month
+} MonthlyDiagnosisData;
+
+typedef struct {
+    int tahun;
+    DiagnosisCount diagnosisCounts[MAX_DIAGNOSIS];
+    int diagnosisCountSize;
+    int total_pasien; // Total patients for this year
+} YearlyDiagnosisData;
+
+
+extern Riwayat records[MAX_RECORDS];
+extern int record_count;
+
+
+#endif // MAINBARU_H
